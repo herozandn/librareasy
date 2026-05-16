2bb87c85e4c689e074f7393815555295c589ec95
henri <henriquezanchin2017@gmail.com>
2026-05-16 00:45:49 -0300

    feat: melhoria classe emprestimo

--- src/main/java/br/com/librareasy/model/Emprestimo.java
+++ src/main/java/br/com/librareasy/model/Emprestimo.java
@@ -1,13 +1,16 @@
 package br.com.librareasy.model;
 
+import java.time.LocalDate;
+
 public class Emprestimo {
-    private static int cont;
+    private static int cont=1;
     private final int idEmprestimo;
     private Exemplar exemplarEmprestado;
     private Usuario usuario;
     private Data dataInicio;
     private Data prazoDevolucao;
     private StatusEmprestimo statusEmprestimo;
+    private Data dataDevolucao;
 
     public Emprestimo(Exemplar exemplarEmprestado, Usuario usuario, Data hoje) {
         this.idEmprestimo = cont++;
@@ -19,9 +22,23 @@ public class Emprestimo {
     }
 
     /**
-     * Calculo de multa
-     * @return
+     * Registra a devolução
+     * @param dataDevolucao data REAL de devolução do exemplar
      */
+    public void registrarDevolucao(Data dataDevolucao){
+        this.dataDevolucao = dataDevolucao;
+        this.statusEmprestimo = StatusEmprestimo.Finalizado;
+    }
+
+    /**
+     * Calcula a multa de atraso
+     * Se houver atrasp a multa é de R$4,00 por dia
+     */
+    public double multa(){
+        if(dataDevolucao==null) return 0.0;
+        long dias = prazoDevolucao.calcularDiferencaEmDias(dataDevolucao);
+        return (dias>0) ? dias*4.0 : 0.0;
+    }
 
     public Exemplar getExemplarEmprestado() {
         return exemplarEmprestado;
